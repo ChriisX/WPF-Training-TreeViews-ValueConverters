d9fd1f4ccf3ae3ed0e99d1e74fd1f4f14eb41b56
Christophe Chichmanian <chris.chich@hotmail.fr>
2019-07-19 15:01:14 +0200

    :sparkles: Added folders detection

--- WPF-Training-TreeViews-ValueConverters/MainWindow.xaml.cs
+++ WPF-Training-TreeViews-ValueConverters/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_Training_TreeViews_ValueConverters
 {
@@ -27,8 +16,78 @@ namespace WPF_Training_TreeViews_ValueConverters
 
             foreach(string drive in Directory.GetLogicalDrives())
             {
-                this.TreeView.Items.Add(drive);
+                TreeViewItem item = new TreeViewItem()
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+
+                // Null item to make the expand icon available
+                item.Items.Add(null);
+
+                // Listen out for item being expanded
+                item.Expanded += Item_Expanded;
+
+                FolderView.Items.Add(item);
             }
+        }
+
+        private void Item_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+
+            // If it doesn't only contain the null item
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+            }
+
+            // Remove null item
+            item.Items.Clear();
+
+            string fullPath = (string)item.Tag;
+
+            List<string> directories = new List<string>();
+
+            try
+            {
+                string[] dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+            }
+            catch { } // Bad practice
+
+            directories.ForEach(directoryPath =>
+            {
+                TreeViewItem subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(directoryPath),
+                    Tag = directoryPath
+                };
+
+                subItem.Items.Add(null);
+
+                subItem.Expanded += Item_Expanded;
+
+                item.Items.Add(subItem);
+            });
+        }
+
+        // Finds the file or folder name from the full path
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return string.Empty;
+            }
+
+            string normalizedPath = path.Replace('/', '\\');
+
+            string[] splitPath = normalizedPath.Split('\\');
+
+            return splitPath[splitPath.Length - 1];
         }
     }
 }
